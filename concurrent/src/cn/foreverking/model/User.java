0a1a256e1bb90896c0cae4054117dcf1ad6832e9
foreverking <455565805@qq.com>
2017-10-15 18:50:34 +0800

    ddd

--- concurrent/src/cn/foreverking/model/User.java
+++ concurrent/src/cn/foreverking/model/User.java
@@ -1,5 +1,30 @@
 package cn.foreverking.model;
 
 public class User {
-
+	private int id;
+	private String name;
+	
+	public User() {
+		id = 0;
+		name = "0";
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + "]";
+	}
+	
 }
--- concurrent/src/cn/foreverking/test/StopThreadUnsafe.java
+++ concurrent/src/cn/foreverking/test/StopThreadUnsafe.java
@@ -1,5 +1,53 @@
 package cn.foreverking.test;
 
-public class StopThreadUnsafe {
+import cn.foreverking.model.User;
 
+public class StopThreadUnsafe {
+	public static User user = new User();
+
+	public static class ChangeObjectThread extends Thread {
+		public void run() {
+			while (true) {
+				synchronized (user) {
+					int v = (int) (System.currentTimeMillis() / 1000);
+					user.setId(v);
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+
+					user.setName(String.valueOf(v));
+				}
+				Thread.yield();
+			}
+		}
+	}
+
+	public static class ReadObjectThread extends Thread {
+		public void run() {
+			while (true) {
+				synchronized (user) {
+					if (user.getId() != Integer.parseInt(user.getName())) {
+						System.out.println(user.toString());
+					}
+					Thread.yield();
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		new ReadObjectThread().start();
+		while (true) {
+			Thread t = new ChangeObjectThread();
+			t.start();
+			try {
+				Thread.sleep(150);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			t.stop();
+		}
+	}
 }
